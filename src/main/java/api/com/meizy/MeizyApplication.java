90f52d4df678ae3636149be1fb235a8bef93eab4
Migsk11 <77Deku@gmail.com>
2026-05-27 11:35:42 -0300

    Versionamento

---
+++ src/main/java/api/com/meizy/MeizyApplication.java
@@ -0,0 +1,13 @@
+package api.com.meizy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeizyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeizyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/com/meizy/MeizyApplicationTests.java
@@ -0,0 +1,13 @@
+package api.com.meizy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeizyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
